cc8d1fa393e6d0756db045f233110d89ba248bf2
OfficialSyntaxx <theofficialsyntaxx@gmail.com>
2026-08-24 13:01:02 -0400

    feat: add hero action animation states

--- unity/Assets/Isoperia/Editor/HeroAnimationSetup.cs
+++ unity/Assets/Isoperia/Editor/HeroAnimationSetup.cs
@@ -14,7 +14,11 @@ namespace Isoperia.EditorTools
             var clips = AssetDatabase.LoadAllAssetsAtPath(ModelPath);
             AnimationClip idle = Find(clips, "Hero_Idle");
             AnimationClip walk = Find(clips, "Hero_Walk");
-            if (idle == null || walk == null) throw new System.InvalidOperationException("Hero clips were not imported");
+            AnimationClip gather = Find(clips, "Hero_Gather");
+            AnimationClip attack = Find(clips, "Hero_Attack");
+            AnimationClip hit = Find(clips, "Hero_Hit");
+            if (idle == null || walk == null || gather == null || attack == null || hit == null)
+                throw new System.InvalidOperationException("Hero clips were not imported");
             AssetDatabase.DeleteAsset(ControllerPath);
             var controller = AnimatorController.CreateAnimatorControllerAtPath(ControllerPath);
             controller.AddParameter("Speed", AnimatorControllerParameterType.Float);
@@ -24,9 +28,27 @@ namespace Isoperia.EditorTools
             var machine = controller.layers[0].stateMachine;
             var idleState = machine.AddState("Idle"); idleState.motion = idle;
             var walkState = machine.AddState("Walk"); walkState.motion = walk;
+            var gatherState = machine.AddState("Gather"); gatherState.motion = gather;
+            var attackState = machine.AddState("Attack"); attackState.motion = attack;
+            var hitState = machine.AddState("Hit"); hitState.motion = hit;
             var toWalk = idleState.AddTransition(walkState); toWalk.hasExitTime = false; toWalk.AddCondition(AnimatorConditionMode.Greater, .05f, "Speed");
             var toIdle = walkState.AddTransition(idleState); toIdle.hasExitTime = false; toIdle.AddCondition(AnimatorConditionMode.Less, .05f, "Speed");
+            AddActionTransition(machine, gatherState, "Gather");
+            AddActionTransition(machine, attackState, "Attack");
+            AddActionTransition(machine, hitState, "Hit");
             AssetDatabase.SaveAssets();
+        }
+
+        private static void AddActionTransition(AnimatorStateMachine machine, AnimatorState state, string trigger)
+        {
+            var enter = machine.AddAnyStateTransition(state);
+            enter.hasExitTime = false;
+            enter.duration = .04f;
+            enter.AddCondition(AnimatorConditionMode.If, 0f, trigger);
+            var exit = state.AddTransition(machine.defaultState);
+            exit.hasExitTime = true;
+            exit.exitTime = .96f;
+            exit.duration = .06f;
         }
 
         private static AnimationClip Find(Object[] assets, string contains)
